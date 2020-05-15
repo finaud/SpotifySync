90e35b4a2b4633c1aac8343dab729e51b75bb5a1
Antoine Finot <antoine.finot@mail.utoronto.ca>
2020-05-14 21:03:13 -0700

    added functioning queue

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ A simple app for listening to Spotify synchronously with friends.
 Run `firebase functions:config:get > .runtimeconfig.json` to save the config variables, and change the value of 
 `redirect` to `"http://localhost:5000/callback"`. This needs to be done only once.
 
+Store the ADC .json file  from the Service Accounts in the folder, and export it:  
+`export GOOGLE_APPLICATION_CREDENTIALS="path/to/key.json"`
+
 To run the emulator, use the following command:  
 `firebase emulators:start`
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,14 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: "https://spotifysync-e4839.firebaseio.com/"
+});
+
+const db = admin.database();
+
+const roomsRef = db.ref('rooms');
 
 /**
  * This is an example of a basic node.js script that performs
@@ -18,6 +28,12 @@ const cookieParser = require('cookie-parser');
 const client_id = functions.config().spotify.id; // Your client id
 const client_secret = functions.config().spotify.secret; // Your secret
 const redirect_uri = functions.config().spotify.redirect; // Redirect URI
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
 
 /**
  * Generates a random string containing numbers and letters
@@ -137,3 +153,49 @@ app.get('/refresh_token', function (req, res) {
 });
 
 exports.app = functions.https.onRequest(app);
+
+exports.playNext = functions.https.onCall((data, context) => {
+    const room = data.room;
+
+    if (!(typeof room === 'string')) {
+        return { message: "Provide a valid room key" };
+    } else {
+        return roomsRef.child(room).once('value').then((snapshot) => {
+            if (snapshot.exists()) {
+                const val = snapshot.val();
+
+                if (val.is_swapping_tracks) {
+                    return { message: 'Server is already swapping tracks' };
+                } else {
+                    roomsRef.child(room + '/is_swapping_tracks').set(true);
+
+                    return roomsRef.child(room + '/queue').orderByKey().limitToFirst(1).once('value').then((snap) => {
+
+                        for (const queue_key in snap.val()) {
+                            data = snap.val()[queue_key];
+
+                            roomsRef.child(room + '/queue/' + queue_key).remove();
+
+                            roomsRef.child(room +'/current_track').update({
+                                uri: data.uri,
+                                added_by: data.added_by,
+                                is_playing: true,
+                                position_ms: 0
+                            });
+                        }
+
+                        return roomsRef.child(room + '/is_swapping_tracks').set(false).then(async () => {
+                            await sleep(1000);
+                            return { message: 'Playing next track' };
+                        });
+                    });
+                }
+            } else {
+                //TODO: prevent users from guessing room keys
+
+                return { message: 'Room does not exist' };
+            }
+
+        });
+    }
+});
